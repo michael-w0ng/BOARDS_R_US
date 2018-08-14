c23ee4c9e223576a8a9ea3af91fa7c4222454a49
Pascal Mies <pascalniklasmies@gmail.com>
2018-08-14 12:11:20 +0200

    created bookings controller, now kind of confused about how to connect the booking view-wise to the other models

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,34 @@
 class BookingsController < ApplicationController
 
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
+    @board = Board.find(params[:board_id])
     @booking = Booking.new
   end
 
   def create
+    @booking.user = current_user
+    @booking.board = Board.find(params[:board_id])
+    @booking = Booking.new(booking_params)
     # @booking = Booking.new(booking_params)
-    # if @booking.save!
-    #   redirect_to booking_path
+    if @booking.save!
+      redirect_to booking_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @dose.destroy
+    redirect_to root_path
   end
 
   private
