233b9e0f8413dbe54dd4b389d58ba4038d2b9c9f
Clara Maria von Wedemeyer <clamajo@vonwedemeyer.com>
2018-08-14 14:49:09 +0200

    reverse routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'boards#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boards
+
+
 end
