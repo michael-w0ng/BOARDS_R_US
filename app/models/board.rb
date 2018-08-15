c5801190cf4eb7c9400aefad6646f42393c5b532
Pascal Mies <pascalniklasmies@gmail.com>
2018-08-15 14:44:12 +0200

    implemented a calendar and cleaned up the booking

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,8 +2,8 @@ class Board < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   belongs_to :user
-  has_many :bookings
-  has_many :reviews
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
